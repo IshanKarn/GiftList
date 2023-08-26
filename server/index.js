4e67cc2daac935fbc29f8163a584d6ba2b16352f
Ishan <ishankarn14@gmail.com>
2023-08-26 15:39:48 +0530

    Updated client and server

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const verifyProof = require('../utils/verifyProof');
+
+const port = 1225;
+
+const app = express();
+app.use(express.json());
+
+// TODO: hardcode a merkle root here representing the whole nice list
+// paste the hex string in here, without the 0x prefix
+const MERKLE_ROOT = "9d209c6cd15e8c6260660976166801a8ca769c067294f58ce7893eb68b8bb236";
+
+app.post('/gift', (req, res) => {
+  // grab the parameters from the front-end here
+  const {proof, name} = req.body;
+
+  // TODO: prove that a name is in the list 
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+  if(isInTheList) {
+    res.send("You got a toy robot!");
+  }
+  else {
+    res.send("You are not on the list :(");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
